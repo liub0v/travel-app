4253989ee2ff346f4673c649357fa6c5dbeb59da
liub0v <kurets2001@mail.com>
2021-10-07 20:58:02 +0300

    add profile screen

--- mobile/src/screens/ProfileScreen/ProfileScreen.js
+++ mobile/src/screens/ProfileScreen/ProfileScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import {logOutIsLoadingSelector} from '../../../redux/selectors/userSelector';
 import {logOutUser} from '../../../redux/actions/AuthActions';
 import {ButtonItem} from '../../components/Buttons/ButtonItem';
-
+import {
+  Avatar,
+  ButtonWrapper,
+  Container,
+  InfoContainer,
+  WhiteText,
+  InfoItem,
+  GreyText,
+  MainInfo,
+  BoldWhiteText,
+} from './Profile.style';
+import avatar from '../../../assets/images/avatarBig.png';
 export const ProfileScreen = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(logOutIsLoadingSelector);
@@ -14,13 +24,42 @@ export const ProfileScreen = () => {
   }
 
   return (
-    <View>
-      <Text>Home page</Text>
-      <ButtonItem
-        isLoading={isLoading}
-        handler={logOutButtonHandler}
-        title={'Log out'}
-      />
-    </View>
+    <Container
+      contentContainerStyle={{flexGrow: 1, justifyContent: 'space-between'}}>
+      <MainInfo>
+        <Avatar source={avatar} />
+        <BoldWhiteText>{'Tanya Edwards'}</BoldWhiteText>
+        <GreyText>{'San Francisco, CA'}</GreyText>
+      </MainInfo>
+      <InfoContainer>
+        <InfoItem>
+          <GreyText>{'Username'}</GreyText>
+          <WhiteText>{'MariotaJin'}</WhiteText>
+        </InfoItem>
+        <InfoItem>
+          <GreyText>{'Email'}</GreyText>
+          <WhiteText>{'tanya.edwards@gmail.com'}</WhiteText>
+        </InfoItem>
+        <InfoItem>
+          <GreyText>{'Phone'}</GreyText>
+          <WhiteText>{'(239) 555-0108'}</WhiteText>
+        </InfoItem>
+        <InfoItem>
+          <GreyText>{'Date of birth'}</GreyText>
+          <WhiteText>{'March 27, 1989'}</WhiteText>
+        </InfoItem>
+        <InfoItem>
+          <GreyText>{'Address'}</GreyText>
+          <WhiteText>{'6391 Elgin St Celina, Delaware'}</WhiteText>
+        </InfoItem>
+      </InfoContainer>
+      <ButtonWrapper>
+        <ButtonItem
+          isLoading={isLoading}
+          handler={logOutButtonHandler}
+          title={'Log out'}
+        />
+      </ButtonWrapper>
+    </Container>
   );
 };
