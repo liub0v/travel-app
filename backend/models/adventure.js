e74e23fc0d03f4152c42846fd8cf06547a45525f
liub0v <kurets2001@mail.com>
2021-10-18 18:00:22 +0300

    feat: adventure model & route

--- backend/models/adventure.js
+++ backend/models/adventure.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 
 const adventureSchema = new mongoose.Schema({
   guideID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Guide",
   },
+  name: String,
+  imageURL: String,
+  summary: String,
+  price: Number,
+  address: String,
+  reviews: Array,
 });
 const Adventure = mongoose.model("Adventure", adventureSchema);
 
-exports.Adventure = Adventure;
+function validateAdventure(destination) {
+  const schema = Joi.object({
+    id: Joi.string(),
+    name: Joi.string().required(),
+    imageURL: Joi.string(),
+    image: Joi.object().optional(),
+    price: Joi.number(),
+    address: Joi.string(),
+  });
+  return schema.validate(destination);
+}
+module.exports.validate = validateAdventure;
+module.exports.Adventure = Adventure;
--- backend/models/guide.js
+++ backend/models/guide.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const { profileInfoSchema } = require("./schemas/profileInfo");
 
 const guideSchema = new mongoose.Schema({
   userID: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+  profileInfo: profileInfoSchema,
+  description: String,
 });
 const Guide = mongoose.model("Guide", guideSchema);
 
