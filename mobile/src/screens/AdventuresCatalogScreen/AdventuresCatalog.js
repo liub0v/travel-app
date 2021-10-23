a2bc957e8bb650f8abcd93f22cc4601aa817e467
liub0v <kurets2001@gmail.com>
2021-10-23 21:31:49 +0300

    feat: adventures catalog UI

--- mobile/src/screens/AdventuresCatalogScreen/AdventuresCatalog.js
+++ mobile/src/screens/AdventuresCatalogScreen/AdventuresCatalog.js
@@ -1,6 +1,56 @@
 import React from 'react';
-import {Text} from 'react-native';
-
+import {FlatList} from 'react-native';
+import {useSelector} from 'react-redux';
+import {destinationsSelector} from '../../../redux/selectors/DestinationSelector';
+import FastImage from 'react-native-fast-image';
+import {Search} from '../../components/Seacrh/Search';
+import {
+  BoldText,
+  FlatListWrapper,
+  ImageItem,
+  ImageWrapper,
+  ItemContainer,
+  MainContainer,
+  NormalText,
+  SearchWrapper,
+  TextItem,
+  TextWrapper,
+  TitleWrapper,
+} from './AdventuresCatalog.style';
+const Destination = ({item}) => {
+  return (
+    <ItemContainer>
+      <ImageWrapper>
+        <FastImage
+          style={{width: 155, height: 155, borderRadius: 16}}
+          blurRadius={5}
+          source={{uri: item.imageURL}}
+        />
+      </ImageWrapper>
+      <TitleWrapper>
+        <NormalText>{'Adventure in '}</NormalText>
+        <BoldText>{item.countryName}</BoldText>
+      </TitleWrapper>
+    </ItemContainer>
+  );
+};
 export const AdventuresCatalog = () => {
-  return <Text>Adventures</Text>;
+  const destinations = useSelector(destinationsSelector);
+  console.log(destinations);
+  return (
+    <MainContainer>
+      <SearchWrapper>
+        <Search placeholder={'Where are you going?'} />
+      </SearchWrapper>
+      <FlatListWrapper>
+        <FlatList
+          numColumns={2}
+          horizontal={false}
+          showsHorizontalScrollIndicator={false}
+          data={destinations}
+          renderItem={Destination}
+        />
+      </FlatListWrapper>
+    </MainContainer>
+  );
 };
